6eb90a7aa315f69410621eb034fd4f1e918b4fb0
V1NNI7 <vinicezar800@hotmail.com>
2021-03-12 17:06:04 -0300

    Projeto-01 Iniciado / CRUD / quase pronto

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+/* const cors = require('cors') */
+const app = express()
+
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+/* const authRouter = require('./routes/auth'); */
+
+app.use(bodyparser.json());
+/* app.use(cors()); */
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+/* app.use('./login', authRouter) */
+
+app.listen(process.env.PORT || 3000 );
---
+++ Backend/routes/user.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const { User } = require('../models');
+
+router.get('/', async (req, res) => {
+    const users =  await User.findAll();
+    res.status(200).json(users);
+});
+
+router.post('/', async (req, res) => {
+    const users = await User.create({
+        username: req.body.username,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+
+    });
+    res.status(200).json(users)
+});
+
+router.put('/:id', async (req, res) => {
+    const users = await User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json('Atualizado com sucesso!')
+});
+
+router.delete('/:id', async (req, res) => {
+    const users = await User.destroy(req.body, {
+        where: {
+        id: req.params.id,
+        }
+    });
+    res.status(200).json(users)
+});
+
+module.exports = router;
